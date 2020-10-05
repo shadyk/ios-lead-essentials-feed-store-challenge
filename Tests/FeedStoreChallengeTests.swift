186f0a2318d8f3fbed5ff9f78e94fd37dd20e478
“Shady <shady.kahaleh@gmail.com>
2020-10-05 23:11:30 +0300

    delivers success inserting On Non Empty Cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -36,14 +36,14 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 
     func test_insert_deliversNoErrorOnEmptyCache() {
         		let sut = makeSUT()
-        
+
         		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
 
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-        //		let sut = makeSUT()
-        //
-        //		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+        		let sut = makeSUT()
+        
+        		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
 
     func test_insert_overridesPreviouslyInsertedCacheValues() {
