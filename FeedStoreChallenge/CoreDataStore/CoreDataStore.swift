b00000ad2bddf333acd83fbc67cd6205e0a2c7e6
“Shady <shady.kahaleh@gmail.com>
2020-10-08 08:58:08 +0300

    replace current cache

--- FeedStoreChallenge/CoreDataStore/CoreDataStore.swift
+++ FeedStoreChallenge/CoreDataStore/CoreDataStore.swift
@@ -34,70 +34,51 @@ public class CoreDataStore : FeedStore{
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        context.perform {
-            do {
-                let managedCache = ManagedCache(context: self.context)
-                var managedFeeds = [ManagedFeed]()
-                for localFeedImage in feed {
-                    let feedImage = ManagedFeed(context: self.context)
-                    feedImage.id = localFeedImage.id
-                    feedImage.descr = localFeedImage.description
-                    feedImage.location = localFeedImage.location
-                    feedImage.url = localFeedImage.url.absoluteString
-                    feedImage.cache = managedCache
-                    managedFeeds.append(feedImage)
-                }
-
-                managedCache.feed = NSOrderedSet(array: managedFeeds)
-                managedCache.timestamp = timestamp
-                try self.context.save()
-                completion(nil)
-            }
-            catch{
-                completion(error)
-            }
+        do {
+            ManagedCache.createNew(feed: feed, timestamp: timestamp, context: self.context)
+            try self.context.save()
+            completion(nil)
         }
-
+        catch{
+            completion(error)
+        }
     }
 
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ManagedCache")
-          do {
-            if let result = try context.fetch(fetchRequest).first {
-                let managedCahe = result as! ManagedCache
-                completion(.found(feed: managedCahe.localFeed, timestamp: managedCahe.timestamp))
+        do {
+            if let managedCache = try ManagedCache.getCurrentCache(in:context) {
+                completion(.found(feed: managedCache.localFeed, timestamp: managedCache.timestamp))
             }
             else {
                 completion(.empty)
             }
 
-          } catch let error as NSError {
+        } catch {
             completion(.failure(error))
-            print("Could not fetch. \(error), \(error.userInfo)")
-          }
+        }
     }
 
     //MARK: - HELPERS
 
-//    private func insertCache(cache:MangagedCache) throws{
-//        let context = container.viewContext
-//        let entity = NSEntityDescription.entity(forEntityName: "Cache", in: context)!
-//        let managedObj = NSManagedObject(entity: entity, insertInto: context)
-//        managedObj.setValue(timestamp, forKeyPath: "timestamp")
-//        managedObj.setValue(NSSet(array: feed), forKeyPath: "feed")
-//        try context.save()
-//    }
-//
-//    private func insertDetails(id:UUID,descr:String?,location:String?,url:String) throws{
-//        let context = container.viewContext
-//        let entity = NSEntityDescription.entity(forEntityName: "Feed", in: context)!
-//        let managedObj = NSManagedObject(entity: entity, insertInto: context)
-//
-//        managedObj.setValue(id, forKeyPath: "id")
-//        managedObj.setValue(descr, forKeyPath: "descr")
-//        managedObj.setValue(location, forKeyPath: "location")
-//        managedObj.setValue(url, forKeyPath: "url")
-//        try context.save()
-//    }
+    //    private func insertCache(cache:MangagedCache) throws{
+    //        let context = container.viewContext
+    //        let entity = NSEntityDescription.entity(forEntityName: "Cache", in: context)!
+    //        let managedObj = NSManagedObject(entity: entity, insertInto: context)
+    //        managedObj.setValue(timestamp, forKeyPath: "timestamp")
+    //        managedObj.setValue(NSSet(array: feed), forKeyPath: "feed")
+    //        try context.save()
+    //    }
+    //
+    //    private func insertDetails(id:UUID,descr:String?,location:String?,url:String) throws{
+    //        let context = container.viewContext
+    //        let entity = NSEntityDescription.entity(forEntityName: "Feed", in: context)!
+    //        let managedObj = NSManagedObject(entity: entity, insertInto: context)
+    //
+    //        managedObj.setValue(id, forKeyPath: "id")
+    //        managedObj.setValue(descr, forKeyPath: "descr")
+    //        managedObj.setValue(location, forKeyPath: "location")
+    //        managedObj.setValue(url, forKeyPath: "url")
+    //        try context.save()
+    //    }
 }
--- FeedStoreChallenge/CoreDataStore/ManagedCache.swift
+++ FeedStoreChallenge/CoreDataStore/ManagedCache.swift
@@ -18,4 +18,37 @@ extension ManagedCache {
         return managedFeeds.map{LocalFeedImage(id: $0.id, description: $0.descr, location: $0.location, url: URL(string:$0.url)!)}
     }
 
+    static func createNew(feed:[LocalFeedImage], timestamp: Date, context: NSManagedObjectContext){
+        cleanCurrentCache(in: context)
+        let managedCache = ManagedCache(context: context)
+        var managedFeeds = [ManagedFeed]()
+        for localFeedImage in feed {
+            let feedImage = ManagedFeed(context: context)
+            feedImage.id = localFeedImage.id
+            feedImage.descr = localFeedImage.description
+            feedImage.location = localFeedImage.location
+            feedImage.url = localFeedImage.url.absoluteString
+            feedImage.cache = managedCache
+            managedFeeds.append(feedImage)
+        }
+        managedCache.feed = NSOrderedSet(array: managedFeeds)
+        managedCache.timestamp = timestamp
+    }
+
+    static func getCurrentCache(in context:NSManagedObjectContext) throws -> ManagedCache?{
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+
+    static func cleanCurrentCache(in context:NSManagedObjectContext){
+        do{
+            try getCurrentCache(in: context).map(context.delete)
+        }
+        catch{
+            print("error in cleaning \(error)")
+        }
+    }
+
+
 }
